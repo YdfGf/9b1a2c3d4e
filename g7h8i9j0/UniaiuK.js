3edbd1be0c6e703cf57979b167abf6d2ab834a06
YdfGf <timhiyanime@gmail.com>
2025-10-25 00:24:50 +0700

    Update UniaiuK.js

--- g7h8i9j0/UniaiuK.js
+++ g7h8i9j0/UniaiuK.js
@@ -1,5 +1,5 @@
-window.videos = [,
-  {
+window.videos = [
+{
   "title": "[4K] Boku no risou 2 jangan terlalu basa nanti kamu bisa stres",
   "aired-start": "2025-10-24",
   "thumbnail": "https://i.postimg.cc/zBqtyFCw/meongking.jpg",
