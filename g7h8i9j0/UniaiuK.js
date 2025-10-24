2172a4f2ced5aff62664e99098360278f2a86bd9
YdfGf <timhiyanime@gmail.com>
2025-10-25 01:08:18 +0700

    Tambah data ke UniaiuK.js

--- g7h8i9j0/UniaiuK.js
+++ g7h8i9j0/UniaiuK.js
@@ -5,5 +5,12 @@ window.videos = [
   "thumbnail": "https://i.postimg.cc/zBqtyFCw/meongking.jpg",
   "direktur": "Admin",
   "eps1": "https://pixeldrain.com/api/file/xHsqKkFo"
+},
+  {
+  "title": "Lo Re: Pako Sukusuku Mizuki-chan",
+  "aired-start": "2025-10-25",
+  "thumbnail": "https://i.postimg.cc/HLR29Rhg/Screenshot-20251025-010513-2.jpg",
+  "direktur": "Admin",
+  "eps1": "https://pixeldrain.com/api/filesystem/4J9pPL9K"
 }
 ];
