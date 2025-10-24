ac03f0608e879bbc5046a7c10aeaeeb0745695ee
YdfGf <timhiyanime@gmail.com>
2025-10-25 00:23:45 +0700

    Tambah data ke UniaiuK.js

--- g7h8i9j0/UniaiuK.js
+++ g7h8i9j0/UniaiuK.js
@@ -1,2 +1,9 @@
-window.videos = [
+window.videos = [,
+  {
+  "title": "[4K] Boku no risou 2 jangan terlalu basa nanti kamu bisa stres",
+  "aired-start": "2025-10-24",
+  "thumbnail": "https://i.postimg.cc/zBqtyFCw/meongking.jpg",
+  "direktur": "Admin",
+  "eps1": "https://pixeldrain.com/api/file/xHsqKkFo"
+}
 ];
